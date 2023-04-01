2ba70c2492f68a232f09d75b794fed53cd19777b
xuhy <1727317079@qq.com>
2023-04-01 11:28:33 +0800

    refactor(公共模块):

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
  * @Author: xuhy 1727317079@qq.com
  * @Date: 2023-03-30 20:20:59
  * @LastEditors: xuhy 1727317079@qq.com
- * @LastEditTime: 2023-03-30 20:31:07
+ * @LastEditTime: 2023-04-01 11:22:54
  * @FilePath: \BMapSVF-Client\tailwind.config.cjs
  * @Description: Tailwind CSS配置
  */
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class' // 关闭深色模式变体
   theme: {
     // 响应设置
     screens: {
@@ -21,7 +21,7 @@ module.exports = {
       lg: "1024px",
       // => @media (min-width: 1024) { ... }
 
-      xl: "1440px",
+      xl: "1440px"
       // => @media (min-width: 1440px) { ... }
     },
     fontSize: {
@@ -30,8 +30,9 @@ module.exports = {
       base: "0.93rem",
       xl: "1.04rem",
       lg: "1.25rem",
-      xxl: "1.67rem",
+      xxl: "1.67rem"
     },
+    // 自定义全局颜色
     colors: {
       transparent: "transparent",
       default: "#ffffff",
@@ -50,14 +51,14 @@ module.exports = {
       "neutral-3": "#666666",
       "neutral-2": "#333333",
       "neutral-1": "#1d1d1d",
-      "neutral-0": "#000000",
+      "neutral-0": "#000000"
     },
-    backgroundColor: (theme) => ({
+    backgroundColor: theme => ({
       ...theme("colors"),
-      "primary-30": "rgba(62, 117, 252, 0.03)",
+      "primary-30": "rgba(62, 117, 252, 0.03)"
     }),
     boxShadow: {
-      default: "0rem 0rem 0.52rem 0.21rem rgba(0, 0, 0, 0.1)",
+      default: "0rem 0rem 0.52rem 0.21rem rgba(0, 0, 0, 0.1)"
     },
     // 拓展覆盖原属性名称
     extend: {
@@ -69,15 +70,15 @@ module.exports = {
         60: "0.6",
         70: "0.7",
         80: "0.8",
-        100: "1",
-      },
-    },
+        100: "1"
+      }
+    }
   },
   variants: {},
   // 支持自定义类名及类内容详见 https://www.tailwindcss.cn/docs/plugins
   plugins: [],
   // 可以在此关闭你不想使用的类
   corePlugins: {
-    container: false,
-  },
+    container: false
+  }
 };
