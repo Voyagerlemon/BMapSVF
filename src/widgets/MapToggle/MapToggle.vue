d2de51f781941c53febad162a0df3fbec15922c9
xuhy <1727317079@qq.com>
2023-04-07 15:54:50 +0800

    refactor(微应用): the MapToggle
    Modified loading data through the config file

--- src/widgets/MapToggle/MapToggle.vue
+++ src/widgets/MapToggle/MapToggle.vue
@@ -2,7 +2,7 @@
  * @Author: xuhy 1727317079@qq.com
  * @Date: 2023-04-06 14:49:50
  * @LastEditors: xuhy 1727317079@qq.com
- * @LastEditTime: 2023-04-07 14:10:36
+ * @LastEditTime: 2023-04-07 15:47:27
  * @FilePath: \BMapSVF-Client\src\widgets\MapToggle\MapToggle.vue
  * @Description: 底图切换组件
 -->
@@ -14,7 +14,7 @@
           <SvgIcon
             v-if="nowBaseLayer"
             className="opacity-80 w-14 h-14"
-            :iconName="nowBaseLayer.icon"
+            :iconName="nowBaseLayer"
           />
         </div>
       </div>
@@ -52,45 +52,59 @@
   </Dropdown>
 </template>
 <script setup>
-import { Dropdown, DropdownMenu } from "view-ui-plus";
-import { ref, reactive } from "vue";
+import { Dropdown, DropdownMenu, Message } from "view-ui-plus";
+import { ref, reactive, onMounted } from "vue";
 import SvgIcon from "@/views/SvgViewer/components/SvgRegister.vue";
-import store from '@/store'
-// 配置文件 ---> MapToggle.hjson
-const baseMapLayers = reactive([
-  { name: "电子地图", icon: "vec", id: 1 },
-  { name: "卫星地图", icon: "img", id: 2 },
-  { name: "混合地图", icon: "img", id: 3 }
-]);
-let nowBaseLayer = ref(
-  { name: "电子地图", icon: "vec" },
-  { name: "卫星地图", icon: "img" },
-  { name: "混合地图", icon: "img" }
-);
+import store from "@/store";
 
+let layerName = ref("");
+let nowBaseLayer = ref("");
+const baseMapLayers = reactive([]);
+const addBaseMap = () => {
+  // 获取到MapToggle配置文件中的baseLayers
+  const { baseLayers } = store.getters["widget/widgetConfig"];
+  // 当前打开的默认底图
+  const curBaseLayer = baseLayers.filter(item => {
+    return item.visible;
+  })[0];
+  if (!curBaseLayer) {
+    Message.error({
+      background: true,
+      content: "Default base layer not configured!",
+      duration: 3
+    });
+  }
+  nowBaseLayer.value = curBaseLayer.icon;
+  layerName.value = curBaseLayer.name;
+  baseMapLayers.push(...baseLayers);
+};
 const isNodeLayer = curBaseLayer => {
   let layer = null;
-
-  if (curBaseLayer.name === nowBaseLayer.value.name) {
+  if (curBaseLayer.name === layerName.value) {
     layer = curBaseLayer;
   }
-
   return layer;
 };
 const changeBaseMap = node => {
   if (node.id === 1) {
     window.map.setMapType(window.BMAP_NORMAL_MAP);
-    nowBaseLayer.value.icon = "vec";
+    nowBaseLayer.value = node.icon;
+    layerName.value = node.name;
   }
   if (node.id === 2) {
     window.map.setMapType(window.BMAP_SATELLITE_MAP);
-    nowBaseLayer.value.icon = "img";
+    nowBaseLayer.value = node.icon;
+    layerName.value = node.name;
   }
   if (node.id === 3) {
     window.map.setMapType(window.BMAP_HYBRID_MAP);
-    nowBaseLayer.value.icon = "img";
+    nowBaseLayer.value = node.icon;
+    layerName.value = node.name;
   }
 };
+onMounted(() => {
+  addBaseMap();
+});
 </script>
 <style lang="scss" scoped>
 $distance: 0.13rem;
