01c7d1d9493bad14b356b121d5dc37cd2ed6b629
xuhy <1727317079@qq.com>
2023-04-28 14:59:07 +0800

    chore(公共模块): package
    removed two packages

--- src/widgets/ScreenExport/ScreenExport.vue
+++ src/widgets/ScreenExport/ScreenExport.vue
@@ -2,49 +2,49 @@
  * @Author: xuhy 1727317079@qq.com
  * @Date: 2023-04-28 09:37:29
  * @LastEditors: xuhy 1727317079@qq.com
- * @LastEditTime: 2023-04-28 11:36:02
+ * @LastEditTime: 2023-04-28 14:53:40
  * @FilePath: \BMapSVF-Client\src\widgets\ScreenExport\ScreenExport.vue
  * @Description: 系统截图
 -->
 <template>
-  <Tooltip content="Screen shot" placement="left" class="absolute bottom-24 right-1">
+  <Tooltip
+    content="Screen shot"
+    placement="left"
+    class="absolute bottom-24 right-1"
+  >
     <div
       class="screen shadow-default cursor-pointer flex items-center justify-center border border-gray-200"
     >
-      <SvgIcon iconName="screen-shot" className="screen-icon" />
+      <SvgIcon
+        iconName="screen-shot"
+        className="screen-icon"
+        @click="clickToImg"
+      />
     </div>
   </Tooltip>
 </template>
 <script setup>
 import SvgIcon from "@/views/SvgViewer/components/SvgRegister.vue";
 import { Tooltip } from "view-ui-plus";
-function takeScreenshot() {
-  const Vcanvas = document.createElement("canvas");
-  Vcanvas
-  const canvas = document.getElementById("screen-export");
-  const video = document.createElement("video");
-  const stream = canvas.captureStream();
+import html2canvas from "html2canvas";
 
-  navigator.mediaDevices
-    .getDisplayMedia({ video: true })
-    .then(displayStream => {
-      video.srcObject = displayStream;
-      video.onloadedmetadata = () => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-        const image = canvas.toDataURL("image/png");
-        const a = document.createElement("a");
-        const event = new MouseEvent("click");
-        a.download = "map.png";
-        a.href = image;
-        a.dispatchEvent(event);
-      };
-    });
-}
+const clickToImg = () => {
+  // 获取整个文档的HTML元素
+  const documentElement = document.documentElement;
+
+  // 调用html2canvas生成canvas
+  html2canvas(documentElement).then(canvas => {
+    // 将canvas转换为图片URL
+    const imgUrl = canvas.toDataURL();
+    const event = new MouseEvent("click");
+    const a = document.createElement("a");
+    a.download = "bmapsvf.png";
+    a.href = imgUrl;
+    a.dispatchEvent(event);
+  });
+};
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .screen {
   width: 2.48rem;
   height: 2.48rem;
@@ -56,17 +56,5 @@ function takeScreenshot() {
     margin-top: 0.33rem;
     fill: #707070;
   }
-}
-
-#screenshot-canvas {
-  position: absolute;
-  top: -99999px;
-  left: -99999px;
-  width: 100%;
-  height: 100%;
-}
-
-video {
-  display: none;
 }
 </style>
